f7fd49a7d0b9976da419bf51f90c6eae3f87e206
GPamplonaSP <gabriel.pereira14@alunos.sc.senac.br>
2025-07-16 15:41:33 -0300

    Aprimoramento da mecanica de andar
    agora, a imagem muda com os botões.

---
+++ Vozes da Igualdade/Assets/Pamplona/PlayerController.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Sprite idleSprite;
+    public Sprite walkSprite;
+    public Sprite upSprite;
+    public Sprite downSprite;
+
+    private Rigidbody2D rb;
+    private SpriteRenderer spriteRenderer;
+    private Vector2 movement;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        // Entrada WASD
+        movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+
+        // Trocar sprite de acordo com direção
+        if (Input.GetKey(KeyCode.W))
+        {
+            spriteRenderer.sprite = upSprite;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            spriteRenderer.sprite = downSprite;
+        }
+        else if (movement.x != 0)
+        {
+            spriteRenderer.sprite = walkSprite;
+
+            // Inverter sprite quando andar para a esquerda
+            if (movement.x < 0)
+                spriteRenderer.flipX = true;
+            else if (movement.x > 0)
+                spriteRenderer.flipX = false;
+        }
+        else
+        {
+            spriteRenderer.sprite = idleSprite;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        // Movimentação com linear velocity
+        rb.velocity = new Vector2(movement.x * moveSpeed, rb.velocity.y);
+    }
+}
