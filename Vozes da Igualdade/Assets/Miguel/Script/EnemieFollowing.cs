f00ffc0df7d13eece46335f809e93c313764aac6
MiguelFerreiraVargas <miguel.vargas@alunos.sc.senac.br>
2025-07-16 15:34:52 -0300

    Inimigo seguindo o player

---
+++ Vozes da Igualdade/Assets/Miguel/Script/EnemieFollowing.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class EnemieFollowing : MonoBehaviour
+{
+    [SerializeField] private float speed = 9;
+
+    [SerializeField] private Transform target;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        void Awake()
+        {
+            target = GameObject.FindGameObjectWithTag("Player").transform;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target == null) return;
+
+        transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+    }
+}
