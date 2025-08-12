05f66110151dc20778758a3f1c92c5b38b7e7e2a
Ricciard0150 <richard.melo@alunos.sc.senac.br>
2025-08-12 15:45:25 -0300

    Mecanica de Inspeção de objetos
    Quando o player chega perto de uma comoda/gaveta/armario, ele pode inspecionar o local e revelar um item/easter egg do jogo

--- Vozes da Igualdade/Assets/Scripts/ContextualizacaoDaHistoria.cs
+++ Vozes da Igualdade/Assets/Scripts/ContextualizacaoDaHistoria.cs
@@ -1,18 +1,33 @@
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class ContextualizacaoDaHistoria : MonoBehaviour
 {
-    [SerializeField] GameObject primeiraPagina; 
+    [SerializeField] GameObject primeiraPagina;
+    [SerializeField] GameObject botaoAveriguar;
+    [SerializeField] GameObject objetoTeste;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-       primeiraPagina.SetActive(false);
+        botaoAveriguar.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        if (other.CompareTag("Player"))
+        {
+            Mostre(); 
+        }
+  
+    }
+    void Mostre ()
+    {
+        botaoAveriguar.SetActive(true);
     }
     public void MostrarCarta ()
     {
@@ -21,5 +36,6 @@ public class ContextualizacaoDaHistoria : MonoBehaviour
     public void fecharCarta ()
     {
         primeiraPagina.SetActive(false);
+        botaoAveriguar.SetActive(false) ;
     }
 }
