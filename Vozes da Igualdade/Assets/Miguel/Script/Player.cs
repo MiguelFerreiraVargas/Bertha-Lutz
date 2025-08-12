08770d45ddea30aa6cd9d62a823ffaefc8cfef9b
MiguelFerreiraVargas <miguel.vargas@alunos.sc.senac.br>
2025-08-12 15:50:18 -0300

    Script Movimentaçao 2.0

--- Vozes da Igualdade/Assets/Miguel/Script/Player.cs
+++ Vozes da Igualdade/Assets/Miguel/Script/Player.cs
@@ -2,19 +2,40 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private float speed = 5f;
+    [SerializeField] private float speed = 5f;
     private float direcao;
-    private Rigidbody rb;
-    private float jumpForce = 3f;
+    private Rigidbody2D rb;
+    private bool isJumping;
+    [SerializeField] private float jumpForce = 3f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody2D>();
+        isJumping = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         direcao = Input.GetAxisRaw("Horizontal");
+
+        rb.linearVelocity = new Vector2(direcao * speed, rb.linearVelocity.y);
+
+        if (direcao > 0 )
+        {
+            transform.localScale = new Vector2(1f,1f);
+        }
+
+        else if (direcao < 0)
+        {
+            transform.localScale = new Vector2(-1f, 1f);
+        }
+
+        if (Input.GetButtonDown("Jump")) && isJumping = false;
+        {
+            rb.linearVelocityY = jumpForce;
+            isJumping = true;
+        }
+        
     }
 }
