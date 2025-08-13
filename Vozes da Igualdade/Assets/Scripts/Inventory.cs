17cd80c882bbb699abe8e60f2d26a8336cbea0f2
Ricciard0150 <richard.melo@alunos.sc.senac.br>
2025-08-13 16:32:24 -0300

    Script do Inventário

--- Vozes da Igualdade/Assets/Scripts/Inventory.cs
+++ Vozes da Igualdade/Assets/Scripts/Inventory.cs
@@ -9,19 +9,23 @@ public class Inventory : MonoBehaviour
     {
         public string name;
         public int id;
+        public string description;
+        public Sprite icon;
+        public int value;
 
-        public Item(string name, int id)
+        public Item(string name, int id, string description, Sprite icon, int value)
         {
             this.name = name;
             this.id = id;
+            this.description = description;
+            this.icon = icon;
+            this.value = value;
         }
     }
 
     public int maxSlots = 10;            
     public List<Item> items = new List<Item>();  
-
-   
-    public void AddItem(string name, int id)
+       public void AddItem(string name, int id, string description, Sprite icon, int value)
     {
         if (items.Count >= maxSlots)
         {
@@ -29,8 +33,9 @@ public class Inventory : MonoBehaviour
             return;
         }
 
-        Item newItem = new Item(name, id);
+        Item newItem = new Item(name, id, description, icon, value);
         items.Add(newItem);
         Debug.Log($"Item adicionado: {name} (ID: {id})");
     }
- }
+
+}
