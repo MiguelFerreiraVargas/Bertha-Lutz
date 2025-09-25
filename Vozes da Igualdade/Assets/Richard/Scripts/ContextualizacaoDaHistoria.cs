32b9174ca5422f0fe83cdeedee78a3256e8856f8
Ricciard0150 <richard.melo@alunos.sc.senac.br>
2025-09-25 10:43:09 -0300

    BugFix

--- Vozes da Igualdade/Assets/Richard/Scripts/ContextualizacaoDaHistoria.cs
+++ Vozes da Igualdade/Assets/Richard/Scripts/ContextualizacaoDaHistoria.cs
@@ -4,23 +4,12 @@ using UnityEngine;
 
 public class ContextualizacaoDaHistoria : MonoBehaviour
 {
-  //[SerializeField] GameObject primeiraPagina;
-  //[SerializeField] GameObject botaoAveriguar;
-  //[SerializeField] GameObject objetoTeste;
+ 
     Item item;
     Inventoryy inventoryy;
     private bool itemId = false; 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        //botaoAveriguar.SetActive(false);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+ 
     void OnTriggerEnter2D(Collider2D other) 
     {
         if (other.CompareTag("Player"))
@@ -37,8 +26,7 @@ public class ContextualizacaoDaHistoria : MonoBehaviour
         int tamanho = inventoryy.items.Count;
         if (tamanho == 1 && inventoryy.items[0].id == 0)
         {
-
-            Debug.Log("LALALALALLAL");
+            Debug.Log("item collected");
         }
     }
 }
