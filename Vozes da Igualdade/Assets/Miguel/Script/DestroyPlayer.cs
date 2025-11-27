38b41f628095899dd09c89b7d0240958b63aad69
MiguelFerreiraVargas <miguel.vargas@alunos.sc.senac.br>
2025-11-27 01:29:07 -0300

    ultimos detalhes

---
+++ Vozes da Igualdade/Assets/Miguel/Script/DestroyPlayer.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DestroyPlayer : MonoBehaviour
+{
+    private string labirintoScene = "Labirinto"; // coloque o nome EXATO da cena
+
+    void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        // Se a nova cena NÃO for o Labirinto → destrói o Player
+        if (scene.name != labirintoScene)
+        {
+            GameObject player = GameObject.FindWithTag("Player");
+            if (player != null)
+            {
+                Destroy(player);
+                Debug.Log("Player destruído ao sair do Labirinto.");
+            }
+        }
+    }
+}
